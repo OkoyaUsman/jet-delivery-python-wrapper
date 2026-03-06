6a42f8630d4862596b86b6fa8679dd17d2570df3
Okoya Usman <usmanokoya10@gmail.com>
2026-03-06 05:41:33 +0100

    pypi upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="jet-delivery-python-wrapper",
+    name="jet-delivery",
     version="0.1.0",
     author="Okoya Usman",
     description="Python wrapper for the Jet Delivery API",
